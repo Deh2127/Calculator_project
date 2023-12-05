c8df26cb282a5b16266dc13b20b8f266c354c740
@EBmelaku <eyob@malaku.com>
2023-12-05 04:05:53 -0500

    js change

---
+++ calc.js
@@ -0,0 +1,12 @@
+//3 parts --- clear, display and calculate
+function clearscreen(){
+    document.getElementById("result").value="";
+    }
+    function display(value){
+        document.getElementById("result").value+=value;
+    }
+    function calculate(){
+        var p =document.getElementById("result").value;
+        var q=eval(p);
+    document.getElementById("result").value=q;
+    }
